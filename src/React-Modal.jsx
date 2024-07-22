f6f045026581c1651245af8d62dffe63de94494c
Matthieu Caron <104826273+matthieucarondev@users.noreply.github.com>
2024-07-22 22:34:56 +0200

    hover close modal

--- src/React-Modal.jsx
+++ src/React-Modal.jsx
@@ -1,20 +1,39 @@
 import React from "react";
 import "./React-Modal.css"; // Ajoutez du style pour la modal
 
-const ReactModal= ({ isOpen, onClose, message, title ,children}) => {
-  if (!isOpen) 
-    return null;
-  
+const ReactModal = ({
+  isOpen,
+  onClose,
+  message,
+  title,
+  children,
+  modalBackground = "#fff",
+  closeButtonBackground = "#000",
+  closeButtonHoverBackground = "#ff0000",
+}) => {
+  if (!isOpen) return null;
+
   return (
     <div className="overlay" onClick={onClose}>
-    <div className="modal" onClick={e => e.stopPropagation()}>
-      <h2>{title}</h2>
-        <spam>{message}</spam>
-      <button className="close-button" onClick={onClose}>X</button>
-      <div className="modal-content">
-        {children}
-      </div> 
-    </div>
+      <div
+        className="modal"
+        onClick={(e) => e.stopPropagation()}
+        style={{ background: modalBackground }}
+      >
+        <h2 className="title-modal">{title}</h2>
+        <span>{message}</span>
+        <button
+          className="close-button"
+          onClick={onClose}
+          style={{
+            '--close-button-background': closeButtonBackground,
+            '--hover-background': closeButtonHoverBackground,
+          }}
+        >
+          X
+        </button>
+        <div className="modal-content">{children}</div>
+      </div>
     </div>
   );
 };
